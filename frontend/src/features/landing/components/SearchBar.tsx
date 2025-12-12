445bebdfcde3d2ed0184297b78127c8f5481c1ca
Nnaemeka10 <egbuchannaemeka2002@gmail.com>
2025-12-12 08:46:34 +0100

    host

--- frontend/src/features/landing/components/SearchBar.tsx
+++ frontend/src/features/landing/components/SearchBar.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@shared/components/Button"
 import { Input } from "@shared/components/Input"
 import { BriefcaseBusiness, Search} from "lucide-react"
+import { useHomeStore } from "../store/usHomeStore"
+import { NAV_LINKS } from "@constants/index"
+import { Link } from "react-router-dom"
 
 
 const SearchBar = () => {
+  const {isMenuOpen, toggleMenu} = useHomeStore();
+
   return (
     <>
       <section className=" text-secondary-900 bg-white w-full shadow-xs py-3 px-6 lg:px-12 2xl:px-32">
@@ -40,7 +45,39 @@ const SearchBar = () => {
       </section>
 
       {/* Mobile menu overlay */}
-      
+      {
+        isMenuOpen && (
+          <div 
+            className="fixed inset-0 bg-black/50 z-60 lg:hidden"
+            onClick={() => toggleMenu()}
+          />
+        )
+      }
+
+      {/* menu dropdown */}
+      <div className={`fixed top-0 right-0 w-[280px] bg-white shadow-2xl z-60 transform transition-transform duration-300 ease-in-out lg:hidden ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+        <div className="flex flex-col h-full px-6 gap-6">
+          {/* Menu links */}
+          {
+            NAV_LINKS.map((l, i) => (
+              <Link 
+                key={i}
+                to={l.href}
+                className="relative text-secondary-900 hover:text-primary-600 transition link-underline-animation text-[0.95rem] border-b border-b-secondary-900"  
+              >
+                {l.label}
+              </Link> 
+            ))
+          }
+
+          {/* buttons */}
+          <div className="flex items-center gap-3">
+            <Button>Create Account</Button>
+            <Button>Sign In</Button>
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
