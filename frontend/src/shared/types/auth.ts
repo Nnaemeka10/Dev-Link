107234eae9c911d1bac51369427ccbe64fe2250a
Konohadev <konohadevv@gmail.com>
2026-04-06 17:43:00 +0100

    i

--- frontend/src/shared/types/auth.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    username: string;
-}
-
-export interface AuthContextType {
-    user: User | null;
-    login: (username: string) => void;
-    logout: () => void;
-}
--- frontend/src/shared/types/job.ts
+++
@@ -1,8 +0,0 @@
-export interface Job {
-    id:string;
-    title: string;
-    company: string;
-    location: string;
-    salary: string;
-    description: string;
-}
